f77067fe225374438e7afeaf24774958f4c509ac
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-10 08:47:46 +0500

    git init

--- learning python/7 project/BinarySearch.py
+++ learning python/7 project/BinarySearch.py
@@ -1,24 +1,19 @@
-list_ = sorted([5, 8, 9, 1, 23, 7, 3, 0, 15])
-user_number = 23
-first = list_[0]
-last = list_[-1]
-middle = list_[(len(list_)//2)-1]
-print(list_)
-print(first)
-print(last)
-print(middle)
-
-answer = 0
-
-while first<=last:
-    if middle==user_number:
-        answer = middle
-    elif middle<user_number:
-        first=middle+1
-    else:
-        last = middle-1
-
-
-
-
-
+list_ = []
+for i in range(1, 100 + 1):
+    list_.append(i)
+low, high = 0, len(list_)
+a = 0
+print('Загадайте число ')
+while low <= high:
+    middle = (high + low) // 2
+    user_number = input(f'Твое число это: {middle}?\nбольше или меньше: ')
+    if user_number.lower() == 'да' or user_number.lower() == 'yes':
+        print(f'Я угадал!')
+        a += 1
+        break
+    elif user_number == "больше":
+        low = middle + 1
+    elif user_number == 'меньше':
+        high = middle - 1
+if a < 1:
+    print('Ты врешь')
