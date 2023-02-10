d146b11ba72a237175efaf6a691940079c463dc0
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-10 19:40:06 +0500

    git init

---
+++ learning python/8 project/TextAdventure.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+screen_size = pygame.display.set_mode((512,512))
+pygame.display.set_caption('Learning Pygame')
+run = True
+
+while run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+
+
+
+
+
+
