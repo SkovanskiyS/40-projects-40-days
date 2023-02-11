bf60dc1db1eaf986832be4b0ae110a31af49f807
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-11 19:04:00 +0500

    git init

--- learning python/8 project/game snake practice/main.py
+++ learning python/8 project/game snake practice/main.py
@@ -1,16 +1,20 @@
-import os
-import time
-
-import pygame
+import os, pygame, time
+import random
 from os import listdir
 
 WIDTH = 600
 HEIGH = 600
-FPS = 30
+FPS = 60
 run = True
+player_speed = 5
+player_x = 100
+player_y = 100
+coin_x = 300
+coin_y = 150
 
 # init
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGH))
 clock = pygame.time.Clock()
 # background
@@ -23,21 +27,56 @@ pygame.display.set_icon(icon)
 pygame.display.set_caption('Snake by Kovanskiy')
 
 walks = []
-path_of_walk = os.getcwd()+'/image/moves'
+path_of_walk = os.getcwd() + '/image/moves'
 for dir2 in listdir(path_of_walk):
-    walks.append(pygame.image.load(path_of_walk+"/"+dir2))
+    walks.append(pygame.image.load(path_of_walk + "/" + dir2))
 
-i = 0
+# set music
+bg_sound = pygame.mixer.Sound('audio/music.wav')
+bg_sound.play()
+bg_sound.set_volume(0.1)
+
+
+def random_coin():
+    # load_img
+    pass
+
+
+coin_img = pygame.image.load('image/coin.png')
+
+pygame.display.update()
 
 while run:
     screen.blit(bg, (0, 0))
-    screen.blit(pygame.transform.scale(walks[i], (67, 120)), (100, 100))
-    if i>3:i=0
-    else: i+=1
-    pygame.display.update()
+    keys = pygame.key.get_pressed()
+    screen.blit(pygame.transform.scale(coin_img, (40, 40)), (coin_x, coin_y))
+    if keys[pygame.K_UP]:
+        player_x -= player_speed
+        print('вверх')
+        screen.blit(pygame.transform.scale(walks[1], (67, 120)), (player_y, player_x))
+    elif keys[pygame.K_DOWN]:
+        player_x += player_speed
+        print('вниз')
+        screen.blit(pygame.transform.scale(walks[2], (67, 120)), (player_y, player_x))
+    elif keys[pygame.K_LEFT]:
+        player_y -= player_speed
+        print('влево')
+        screen.blit(pygame.transform.scale(walks[3], (67, 120)), (player_y, player_x))
+    elif keys[pygame.K_RIGHT]:
+        player_y += player_speed
+        print('направо')
+        screen.blit(pygame.transform.scale(walks[4], (67, 120)), (player_y, player_x))
+    else:
+        screen.blit(pygame.transform.scale(walks[0], (67, 120)), (player_y, player_x))
+
+    # check for coin
+    if coin_x == player_x or coin_y == player_y:
+        coin_x += random.randint(30, 400)
+        coin_y += random.randint(30, 400)
 
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             run = False
     clock.tick(FPS)
+    pygame.display.update()
 pygame.quit()
