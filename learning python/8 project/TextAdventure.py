90863117d2457faae5ce760f6c9369f9578729bc
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-11 16:30:41 +0500

    git init

--- learning python/8 project/TextAdventure.py
+++ learning python/8 project/TextAdventure.py
@@ -3,27 +3,30 @@ import pygame
 pygame.init()
 W = 512
 H = 512
-screen = pygame.display.set_mode((512, 512))
-img = pygame.image.load('img/street.jpg')
+screen = pygame.display.set_mode((W, H))
+img = pygame.image.load('img/randomPic2.png')
 pygame.display.set_icon(img)
 pygame.display.set_caption('Glitc-in-the-Head')  # GITH
 run = True
 square = pygame.Surface((50, 50))
+
 # square.fill('Blue')
 # screen.blit(square, (W / 2-25, H / 2-25))
 # pygame.display.update()
 
 text_fonts = pygame.font.Font("fonts/Roboto-Bold.ttf",40)
-text_surface = text_fonts.render("Hello, World",True,'Green')
+text_surface = text_fonts.render("Hello, World",True,'White','Green')
 
 while run:
-    screen.blit(img,(0,0))
+    screen.blit(img,dest=(20,30))
+    screen.blit(text_surface,(30,30))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
     pygame.display.update()
+
+
 pygame.quit()
 
 
--- learning python/8 project/sprites.py
+++
@@ -1,6 +0,0 @@
-import pygame
-import main
-
-class Player(pygame.sprite.Sprite):
-    def init(self):
-        pygame.sprite.Sprite.__init__(self)
