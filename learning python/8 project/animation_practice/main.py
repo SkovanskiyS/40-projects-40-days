e94b5c087a1afe1759a373dbf8cd6719e32a2cbe
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-15 22:59:57 +0500

    git init

--- learning python/8 project/animation_practice/main.py
+++ learning python/8 project/animation_practice/main.py
@@ -2,28 +2,39 @@ import random
 import time
 
 import pygame
+import pymunk.pygame_util
 
+WIDTH = 700
+HEIGHT = 700
 pygame.init()
-screen = pygame.display.set_mode((700, 700))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 X = 100
 Y = 600
-speed = 10
+speed = 1
 models = []
 clock_ = pygame.time.Clock()
-FPS = 60
+FPS = 12312
 W_RECT = 60
 H_RECT = 4
 animation_count = 0
 
+pymunk.pygame_util.positive_y_is_up = False
+#переменные Pymunk
+
+draw_options = pymunk.pygame_util.DrawOptions(screen)
 for i in range(1, 10):
     models.append(pygame.image.load(f'images/image_part_00{i}.png'))
 models.append(pygame.image.load(f'images/image_part_0010.png'))
+ball_x = 20
+ball_y = 20
+
 
 while running:
 
     get_pressed = pygame.key.get_pressed()
     screen.fill((0, 0, 0))
+
     if X <= -70:
         X = 700
     elif X >= 700:
@@ -34,7 +45,7 @@ while running:
     elif get_pressed[pygame.K_LEFT]:
         X -= speed
     pygame.draw.rect(screen, "White", (X, Y, W_RECT, H_RECT))
-    pygame.display.update()
+    pygame.display.flip()
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             running = False
