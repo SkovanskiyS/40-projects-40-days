26dd2e0a1be90b01a7405b2dd28221af8980ccda
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-02-18 18:20:54 +0500

    git init

--- learning python/8 project/two-sides/Rocket.py
+++ learning python/8 project/two-sides/Rocket.py
@@ -4,17 +4,14 @@ import pygame
 class Rocket:
     width = 10
     height = 30
-
     def __init__(self, surface, x, y, speed):
         self.surf = surface
         self.r_x = x
         self.r_y = y
         self.speed = speed
-
     def update(self):
         self.r_y -= self.speed
         if self.r_y<0:
             self.r_y = 300
-
     def draw(self,screen,color):
         pygame.draw.rect(screen,color,(self.r_x,self.r_y,self.width,self.height))
