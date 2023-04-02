35c72f5a3b9fc89d013a6cd38a182653d9eab561
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-04-02 17:19:23 +0500

    git init

--- learning python/8 project/sprite_practice/main.py
+++ learning python/8 project/sprite_practice/main.py
@@ -1,38 +1,42 @@
+# import pygame
+# from pygame.color import THECOLORS
+#
+# #initialize
+# pygame.init()
+# W = 600
+# H = 600
+# FPS = 60
+# screen = pygame.display.set_mode((W, H))
+# pygame.display.set_caption('hi')
+# clock = pygame.time.Clock()
+# screen.fill(THECOLORS['antiquewhite4'])
+# pygame.display.update()
+#
+# # sprites
+# background_image = pygame.image.load("Tile_25-512x512.png")
+# run = True
+#
+# # load image
+#
+# while run:
+#     for event in pygame.event.get():
+#         pressed_key = pygame.key.get_pressed()
+#         if event.type == pygame.QUIT or pressed_key[pygame.K_ESCAPE]:
+#             run = False
+#     pygame.display.update()
+#
+#     clock.tick(FPS)
+#
+# pygame.quit()
+
 import pygame
 
-pygame.init()
-W = 1024
-H = 900
-screen = pygame.display.set_mode((W, H))
-pygame.display.set_caption('hi')
-
-# sprites
-
-background_image = pygame.image.load("Tile_25-512x512.png")
-bg = pygame.transform.scale(background_image,(screen.get_width(),screen.get_height()))
-run = True
-
-
-#load image
-first = pygame.image.load('player/1.png')
-sec = pygame.image.load('player/2.png')
-th = pygame.image.load('player/3.png')
-f = pygame.image.load('player/4.png')
-fivth = pygame.image.load('player/5.png')
-six = pygame.image.load('player/6.png')
-
-while run:
-    screen.blit(bg, (0, 0))
-    for event in pygame.event.get():
-        pressed_key = pygame.key.get_pressed()
-        if event.type == pygame.QUIT or pressed_key[pygame.K_ESCAPE]:
-            run = False
+screen = pygame.display.set_mode((500,500))
+running = True
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
     pygame.display.update()
 
 pygame.quit()
-
-
-
-
-
-
