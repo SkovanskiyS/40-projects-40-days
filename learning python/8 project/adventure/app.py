ba599f6932408fb5e91e5ae3597a3bbb1213ab67
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-04-12 20:12:15 +0500

    git init

--- learning python/8 project/adventure/app.py
+++ learning python/8 project/adventure/app.py
@@ -12,7 +12,6 @@ running = True
 pygame.display.set_caption('Adventure Game')
 clock = pygame.time.Clock()
 
-
 # image surface
 first_surface = pygame.image.load(r'objects/statue.png')
 transformed = pygame.transform.scale(first_surface, (80, 127))
@@ -21,20 +20,43 @@ transformed = pygame.transform.scale(first_surface, (80, 127))
 font_text = pygame.font.Font(None, 50)
 text_surface = font_text.render('Your score: ', True, 'Red')
 
-
+# player
+player_sprite = [pygame.image.load(fr'objects/player/stay{sprite}.png') for sprite in range(1, 8)]
+j = 0
+player_x_pos = 0
+player_y_pos = 0
 while running:
-    score_text = font_text.render(str(score), True, 'Red')
     screen.fill((0, 255, 255))
     pressed = pygame.key.get_pressed()
+    if j >= 7: j = 0
+
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             running = False
-        elif pressed[pygame.K_SPACE]:
+        elif pressed[pygame.K_w]:
             score += 1
+        elif pressed[pygame.K_s]:
+            score -= 1
+
+    # show up
     screen.blit(transformed, (200, 100))
     screen.blit(text_surface, (350, 10))
-    screen.blit(score_text, (550, 10))
+    screen.blit(font_text.render(str(score), True, 'Red'), (550, 10))
+
+    # controller
+    if pressed[pygame.K_d] and player_x_pos <= 830:
+        player_x_pos += 5
+    elif pressed[pygame.K_a] and player_x_pos >= -22:
+        player_x_pos -= 5
+    elif pressed[pygame.K_s] and player_y_pos <= 500:
+        player_y_pos += 5
+    elif pressed[pygame.K_w] and player_y_pos >= -35:
+        player_y_pos -= 5
+
+    screen.blit(pygame.transform.scale(player_sprite[j], (100, 100)), (player_x_pos, player_y_pos))
+
     pygame.display.update()
     clock.tick(FPS)
+    j += 1
 
 pygame.quit()
