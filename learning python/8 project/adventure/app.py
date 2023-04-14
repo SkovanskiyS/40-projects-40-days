3e1f5ee1d5f0b2ab070e64a50e5df80b86f8e8f0
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-04-14 19:11:28 +0500

    git init

--- learning python/8 project/adventure/app.py
+++ learning python/8 project/adventure/app.py
@@ -1,62 +1,72 @@
-import time
-
 import pygame
 
 pygame.init()
 W = 900
 H = 600
 FPS = 60
-score = 0
+SCORE = 0
 screen = pygame.display.set_mode((W, H))
 running = True
 pygame.display.set_caption('Adventure Game')
 clock = pygame.time.Clock()
 
 # image surface
-first_surface = pygame.image.load(r'objects/statue.png')
-transformed = pygame.transform.scale(first_surface, (80, 127))
+statue_surface = pygame.image.load(r'objects/statue.png').convert_alpha()
+transformed_surface = pygame.transform.scale(statue_surface, (80, 127))
+statue_rect = transformed_surface.get_rect(topleft=(100, 300))
 
 # text
 font_text = pygame.font.Font(None, 50)
 text_surface = font_text.render('Your score: ', True, 'Red')
+text_rect = text_surface.get_rect(topleft=(350, 10))
 
 # player
-player_sprite = [pygame.image.load(fr'objects/player/stay{sprite}.png') for sprite in range(1, 8)]
+player_sprite = [pygame.image.load(fr'objects/player/stay{sprite}.png').convert_alpha() for sprite in range(1, 8)]
 j = 0
-player_x_pos = 0
-player_y_pos = 0
+player_rect = pygame.transform.scale(player_sprite[0], (70, 70)).get_rect(center=(0, 0))
+
 while running:
-    screen.fill((0, 255, 255))
-    pressed = pygame.key.get_pressed()
-    if j >= 7: j = 0
-
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
-        elif pressed[pygame.K_w]:
-            score += 1
-        elif pressed[pygame.K_s]:
-            score -= 1
-
-    # show up
-    screen.blit(transformed, (200, 100))
-    screen.blit(text_surface, (350, 10))
-    screen.blit(font_text.render(str(score), True, 'Red'), (550, 10))
-
+    if j >= 7: j = 0
+    p_surface = pygame.transform.scale(player_sprite[j], (70, 70))
+    pressed = pygame.key.get_pressed()
+    print(pressed)
     # controller
-    if pressed[pygame.K_d] and player_x_pos <= 830:
-        player_x_pos += 5
-    elif pressed[pygame.K_a] and player_x_pos >= -22:
-        player_x_pos -= 5
-    elif pressed[pygame.K_s] and player_y_pos <= 500:
-        player_y_pos += 5
-    elif pressed[pygame.K_w] and player_y_pos >= -35:
-        player_y_pos -= 5
+    if pressed[pygame.K_d] and player_rect.right <= 850:
+        player_rect.left += 5
+    elif pressed[pygame.K_a] and player_rect.left >= -22:
+        player_rect.right -= 5
+    elif pressed[pygame.K_s] and player_rect.bottom <= 550:
+        player_rect.top += 5
+    elif pressed[pygame.K_w] and player_rect.top >= -25:
+        player_rect.bottom -= 5
 
-    screen.blit(pygame.transform.scale(player_sprite[j], (100, 100)), (player_x_pos, player_y_pos))
+    # background
+    screen.blit(pygame.image.load(r'objects/bg/bg.png').convert(), (0, 0))
+    # show up
+    screen.blit(p_surface, player_rect)
+    screen.blit(text_surface, text_rect)
+    screen.blit(font_text.render(str(SCORE), True, 'Red'), (550, 10))
+    screen.blit(transformed_surface, statue_rect)
+
+    # ---- DRAWING ------
+    # player rect
+    pygame.draw.rect(screen, 'white', player_rect, 1)
+
+    # statue draw
+    pygame.draw.rect(screen, 'white', statue_rect, 1)
+
+    # player line draw
+    pygame.draw.aaline(screen, 'Red', player_rect.center, pygame.mouse.get_pos(), 10)
+
+    # text draw
+    pygame.draw.rect(screen, 'white', text_rect, 1)
 
     pygame.display.update()
     clock.tick(FPS)
+    pygame.display.set_caption("Fps: " + str(round(clock.get_fps())))
     j += 1
 
 pygame.quit()
