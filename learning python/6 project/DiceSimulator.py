0a50148ff296fe133a727fd607d86823ad087f39
John <70656337+SkovanskiyS@users.noreply.github.com>
2023-05-29 18:15:18 +0500

    git init

--- learning python/6 project/DiceSimulator.py
+++ learning python/6 project/DiceSimulator.py
@@ -1,6 +1,5 @@
 import random
 make_random_throw = True
 while make_random_throw:
-    makeIt = input('Do you want to start?: \nyes or no: ')
-    if makeIt == 'yes': print(f"You got: \n\t\n| {random.randint(1,6)} |\n")
+    if input('Do you want to start?: \nyes or no: ') == 'yes': print(f"You got: \n\t\n| {random.randint(1,6)} | {random.randint(1,6)} | \n")
     else: break
--- learning python/9 project/alarm.py
+++ learning python/9 project/alarm.py
@@ -1,2 +1,25 @@
+import math
+import time
 import tkinter as tk
+import datetime
+import string
 
+display = tk.Tk()
+
+display.title('hello world')
+
+rightOne = '3.1455926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+shouldBeChecked = '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+limit = input('Enter the limit length of the number PI')
+def filter(usersPI):
+    clean_text = ''.join(char for char in usersPI if char not in string.punctuation).replace(' ', '').replace('\t', '')
+    return clean_text
+
+def check(userNumber):
+    userPi = filter(userNumber)
+    for i, j in enumerate(rightOne):
+        if j != shouldBeChecked[i]:
+            print(f'Неверно: computer {j} and user\'s {shouldBeChecked[i]}')
+        else:
+            print(f'Верно: computer {j} and user\'s {shouldBeChecked[i]}')
+print(math.pi)
